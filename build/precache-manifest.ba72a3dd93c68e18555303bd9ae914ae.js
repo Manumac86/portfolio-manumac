7d3374577fca26d2a218a5000c215712f72160de
Emmanuel Martinez <Emmanuel.X.Martinez.-nd@disney.com>
2019-03-06 13:24:09 -0300

    First Prod build

---
+++ build/precache-manifest.ba72a3dd93c68e18555303bd9ae914ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7b87928d580cca969f54b7c673606d0a",
+    "url": "/static/media/avatar.7b87928d.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ac8f7709ac4964076f9f",
+    "url": "/static/js/main.ac8f7709.chunk.js"
+  },
+  {
+    "revision": "62507dd0e61ef6a60d15",
+    "url": "/static/js/2.62507dd0.chunk.js"
+  },
+  {
+    "revision": "ac8f7709ac4964076f9f",
+    "url": "/static/css/main.4ca95e64.chunk.css"
+  },
+  {
+    "revision": "ad7ac39648d4c0bee7ed227efdf42c8e",
+    "url": "/index.html"
+  }
+];
